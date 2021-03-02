5963f8378656ee70291f8fdf63498d055486087b
晓东 <1052672227@qq.com>
2021-03-02 19:21:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git Lean-SSRPlus https://github.com/immortalwrt/Lean-SSRPlus' feeds.conf.default
+sed -i '$a src-git SSRPlus https://github.com/immortalwrt/Lean-SSRPlus' feeds.conf.default
