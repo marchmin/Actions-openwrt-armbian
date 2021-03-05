64bbc81d8005d3d71df911dcd2674cf9d848f805
晓东 <1052672227@qq.com>
2021-03-05 23:51:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git xd https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git xd https://github.com/fw876/helloworld' feeds.conf.default
