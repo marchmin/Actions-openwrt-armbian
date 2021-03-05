eafcc33f0e0293d4ea2a0d404bae41a64f73c7aa
晓东 <1052672227@qq.com>
2021-03-06 07:53:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git xd https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git xd https://github.com/shiyu1314/helloworld' feeds.conf.default
