859b9e15a312bbe180683bb6ce980cdb54cdf0d4
晓东 <1052672227@qq.com>
2021-07-19 10:00:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #sed -i '$a src-git xd https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git xd https://github.com/shiyu1314/luci-app-turboacc' feeds.conf.default
+sed -i '$a src-git xd https://github.com/liuran001/openwrt-packages' feeds.conf.default
