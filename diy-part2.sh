4c2a2aeeec9977bc94c1b32b5e9df5f5b30edd12
晓东 <1052672227@qq.com>
2022-04-08 09:57:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
+sed -i 's/shadowsocksr/passwall/g' package/luci-theme-neobird/luasrc/view/themes/neobird/header.htm
