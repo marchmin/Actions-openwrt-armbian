babfcc8eb8f2bf03e1f7cd50f85487741e3df5d1
晓东 <1052672227@qq.com>
2022-04-16 19:16:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 sed -i '$a src-git xd https://github.com/shiyu1314/luci-theme-neobird' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git smalm https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git smalm https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
