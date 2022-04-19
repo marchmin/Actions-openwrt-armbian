cc9bf023e051866fda27eb5c6456093b3b3d1c20
晓东 <1052672227@qq.com>
2022-04-19 23:10:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 sed -i '$a src-git xd https://github.com/shiyu1314/luci-theme-neobird' feeds.conf.default
-sed -i '$a src-git small https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
+sed -i '$a src-git https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git smalm https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
